4df6daf2bc8be1b87fbde6762729cd70f1536266
Rogerio Junior <junior.rogerio8@gmail.com>
2021-09-30 22:36:40 -0300

    Improve functions names

--- src/gpio.cpp
+++ src/gpio.cpp
@@ -2,37 +2,35 @@
 
 #include <iostream>
 
-using fn = void (*)();
+using void_function = void (*)();
 
 const int MAX{101};
 
 map<int, pair<string, string>> sensors;
-static array<fn, MAX> Fs;
+static array<void_function, MAX> pin_event_handler;
 
-#define TXT(x) handle_event_##x
-
-void handle_event_base(pair<string, string> info)
+void handle_event(pair<string, string> info)
 {
     cout << info.first << " " << info.second << endl;
 }
 
-template <int K>
-auto genF()
+template <int pin>
+auto callback_generator()
 {
     return []()
-    { handle_event_base(sensors[K]); };
+    { handle_event(sensors[pin]); };
 }
 
 template <int N>
-void buildFs()
+void instantiate_callbacks()
 {
     if constexpr (N > 0)
     {
-        Fs[N] = genF<N>();
-        buildFs<N - 1>();
+        pin_event_handler[N] = callback_generator<N>();
+        instantiate_callbacks<N - 1>();
     }
     else
-        Fs[0] = genF<0>();
+        pin_event_handler[0] = callback_generator<0>();
 }
 
 GPIO::GPIO(string sensors_json_path)
@@ -48,14 +46,15 @@ GPIO::GPIO(string sensors_json_path)
     ifstream sfs(sensors_json_path);
     jf = json::parse(sfs);
 
-    buildFs<MAX>();
+    instantiate_callbacks<MAX>();
 
     for (auto &sensor_info : jf["outputs"])
     {
         sensors[sensor_info["gpio"]] = {sensor_info["tag"], sensor_info["type"]};
 
         pinMode(gpio_to_wiringpi_pin[sensor_info["gpio"]], OUTPUT);
-        wiringPiISR(gpio_to_wiringpi_pin[sensor_info["gpio"]], INT_EDGE_BOTH, Fs[sensor_info["gpio"].get<int>()]);
+        wiringPiISR(gpio_to_wiringpi_pin[sensor_info["gpio"]], INT_EDGE_BOTH,
+                    pin_event_handler[sensor_info["gpio"].get<int>()]);
     }
 
     for (auto &sensor_info : jf["inputs"])
@@ -63,7 +62,8 @@ GPIO::GPIO(string sensors_json_path)
         sensors[sensor_info["gpio"]] = {sensor_info["tag"], sensor_info["type"]};
 
         pinMode(gpio_to_wiringpi_pin[sensor_info["gpio"]], INPUT);
-        wiringPiISR(gpio_to_wiringpi_pin[sensor_info["gpio"]], INT_EDGE_BOTH, Fs[sensor_info["gpio"].get<int>()]);
+        wiringPiISR(gpio_to_wiringpi_pin[sensor_info["gpio"]], INT_EDGE_BOTH,
+                    pin_event_handler[sensor_info["gpio"].get<int>()]);
     }
 }
 
